877437d891b0d708e84b2ab2311e27b51d4f7f68
Nicolas Duminil <nicolas.duminil@simplex-software.fr>
2025-03-03 12:26:43 +0100

    Testing Discovery and Authentication request

--- front-end/src/main/java/fr/simplex_software/iam/fe/view/OpenIdConnectView.java
+++ front-end/src/main/java/fr/simplex_software/iam/fe/view/OpenIdConnectView.java
@@ -1,60 +1,56 @@
 package fr.simplex_software.iam.fe.view;
 
-import fr.simplex_software.iam.domain.schema.*;
 import fr.simplex_software.iam.fe.service.*;
+import io.quarkus.runtime.annotations.*;
 import jakarta.enterprise.context.*;
 import jakarta.faces.application.*;
 import jakarta.faces.context.*;
 import jakarta.faces.view.*;
 import jakarta.inject.*;
-import jakarta.json.*;
 import jakarta.json.bind.*;
 import jakarta.servlet.http.*;
-import jakarta.ws.rs.*;
 import jakarta.ws.rs.client.*;
 import jakarta.ws.rs.core.*;
 import org.apache.commons.lang3.*;
 import org.eclipse.microprofile.config.inject.*;
 import org.eclipse.microprofile.rest.client.inject.*;
-import org.primefaces.*;
+import org.primefaces.event.*;
 
 import java.io.*;
 import java.util.*;
 
 @Named
 @ViewScoped
+@RegisterForReflection(serialization = true)
 public class OpenIdConnectView implements Serializable
 {
   @RestClient
   private IamServiceClient iamServiceClient;
   @Inject
-  private FacesContext facesContext;
+  FacesContext facesContext;
   @ConfigProperty(name = "quarkus.oidc.auth-server-url")
   String issuer;
   @ConfigProperty(name = "quarkus.oidc.client-id")
   String clientId;
-  private String scope;
-  private String prompt;
-  private String maxAge;
-  private String loginHint;
-  private String code;
-  private String discoveryJson;
-  private String idToken;
-
-  private String accessToken;
-  private String refreshToken;
   private Map<String, Object> discovery;
+  private String discoveryJson;
+  private boolean showDiscoveryJson;
   private String authenticationRequest;
-  private String metaData = null;
+  private boolean showAuthenticationRequest;
 
   public String getClientId()
   {
     return clientId;
   }
 
-  public void setClientId(String clientId)
+  public boolean isShowDiscoveryJson()
   {
-    this.clientId = clientId;
+    return showDiscoveryJson;
+  }
+
+  public boolean isShowAuthenticationRequest()
+  {
+    return showAuthenticationRequest;
   }
 
   public String getIssuer()
@@ -62,109 +58,9 @@ public class OpenIdConnectView implements Serializable
     return issuer;
   }
 
-  public void setIssuer(String issuer)
-  {
-    this.issuer = issuer;
-  }
-
-  public String getScope()
-  {
-    return scope;
-  }
-
-  public void setScope(String scope)
-  {
-    this.scope = scope;
-  }
-
-  public String getPrompt()
-  {
-    return prompt;
-  }
-
-  public void setPrompt(String prompt)
-  {
-    this.prompt = prompt;
-  }
-
-  public String getMaxAge()
-  {
-    return maxAge;
-  }
-
-  public void setMaxAge(String maxAge)
-  {
-    this.maxAge = maxAge;
-  }
-
-  public String getLoginHint()
-  {
-    return loginHint;
-  }
-
-  public void setLoginHint(String loginHint)
-  {
-    this.loginHint = loginHint;
-  }
-
-  public String getCode()
-  {
-    return code;
-  }
-
-  public void setCode(String code)
-  {
-    this.code = code;
-  }
-
   public String getDiscoveryJson()
   {
     return discoveryJson;
-  }
-
-  public void setDiscoveryJson(String discoveryJson)
-  {
-    this.discoveryJson = discoveryJson;
-  }
-
-  public String getIdToken()
-  {
-    return idToken;
-  }
-
-  public void setIdToken(String idToken)
-  {
-    this.idToken = idToken;
-  }
-
-  public String getAccessToken()
-  {
-    return accessToken;
-  }
-
-  public void setAccessToken(String accessToken)
-  {
-    this.accessToken = accessToken;
-  }
-
-  public String getRefreshToken()
-  {
-    return refreshToken;
-  }
-
-  public void setRefreshToken(String refreshToken)
-  {
-    this.refreshToken = refreshToken;
-  }
-
-  public Map<String, Object> getDiscovery()
-  {
-    return discovery;
-  }
-
-  public void setDiscovery(Map<String, Object> discovery)
-  {
-    this.discovery = discovery;
   }
 
   public String getAuthenticationRequest()
@@ -172,71 +68,70 @@ public class OpenIdConnectView implements Serializable
     return authenticationRequest;
   }
 
-  public void setAuthenticationRequest(String authenticationRequest)
-  {
-    this.authenticationRequest = authenticationRequest;
-  }
-
-  public boolean isMetadata()
-  {
-    return metaData != null;
-  }
-
   public void loadDiscovery()
   {
-    try (Client client = ClientBuilder.newClient())
-    {
-      WebTarget target = client.target(issuer + "/.well-known/openid-configuration");
-      discovery = target.request(MediaType.APPLICATION_JSON).get(Map.class);
-      discoveryJson = JsonbBuilder.create().toJson(discovery);
-    }
-    catch (Exception e)
-    {
-      FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR,
-        "Error loading discovery document", e.getMessage());
-      facesContext.addMessage(null, message);
-    }
+    System.out.println ("### Entry loadDiscovery(): discoveryJson is "  + (StringUtils.isEmpty(discoveryJson) ? "empty" : "not empty" + " and showDiscoveryJson is " + showDiscoveryJson));
+    if (StringUtils.isEmpty(discoveryJson))
+      try (Client client = ClientBuilder.newClient())
+      {
+        WebTarget target = client.target(issuer + "/.well-known/openid-configuration");
+        discovery = target.request(MediaType.APPLICATION_JSON).get(Map.class);
+        discoveryJson = JsonbBuilder.create(new JsonbConfig()
+          .withFormatting(true))
+          .toJson(discovery);
+        showDiscoveryJson = true;
+      }
+      catch (Exception e)
+      {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+          "Error loading discovery document", e.getMessage());
+        facesContext.addMessage(null, message);
+        showDiscoveryJson = false;
+      }
+    else
+      showDiscoveryJson = !showDiscoveryJson;
+    System.out.println ("### Exit loadDiscovery(): discoveryJson is "  + (StringUtils.isEmpty(discoveryJson) ? "empty" : "not empty" + " and showDiscoveryJson is " + showDiscoveryJson));
   }
 
-  public String generateAuthenticationRequest()
+  public void generateAuthenticationRequest()
   {
+    System.out.println(">>> Entry generateAuthenticationRequest(): discovery is " + (discovery == null || discovery.isEmpty() ? "empty" : "not empty")
+      + " authenticationRequest is " + (StringUtils.isEmpty(authenticationRequest) ? "empty" : "not empty")
+      + " showAuthenticationRequest is " + showAuthenticationRequest);
     if (discovery == null || discovery.isEmpty())
     {
       FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR,
         "Discovery document not loaded", null);
       facesContext.addMessage(null, message);
-      return null;
+      showAuthenticationRequest = false;
     }
-
-    String authEndpoint = (String) discovery.get("authorization_endpoint");
-    String redirectUri = getRedirectUri();
-
-    UriBuilder builder = UriBuilder.fromUri(authEndpoint)
-      .queryParam("client_id", clientId)
-      .queryParam("response_type", "code")
-      .queryParam("redirect_uri", redirectUri);
-
-    if (StringUtils.isNotBlank(scope))
+    else
     {
-      builder.queryParam("scope", scope);
+      if (StringUtils.isEmpty(authenticationRequest))
+      {
+        String authEndpoint = (String) discovery.get("authorization_endpoint");
+        String redirectUri = getRedirectUri();
+        UriBuilder builder = UriBuilder.fromUri(authEndpoint)
+          .queryParam("client_id", clientId)
+          .queryParam("response_type", "code")
+          .queryParam("redirect_uri", redirectUri);
+        authenticationRequest = builder.build().toString();
+        showAuthenticationRequest = true;
+      }
+      else
+        showAuthenticationRequest = !showAuthenticationRequest;
     }
-    if (StringUtils.isNotBlank(prompt))
-    {
-      builder.queryParam("prompt", prompt);
-    }
-    if (StringUtils.isNotBlank(maxAge))
-    {
-      builder.queryParam("max_age", maxAge);
-    }
-    if (StringUtils.isNotBlank(loginHint))
-    {
-      builder.queryParam("login_hint", loginHint);
-    }
-
-    return builder.build().toString();
+    System.out.println(">>> Exit generateAuthenticationRequest(): discovery is " + (discovery == null || discovery.isEmpty() ? "empty" : "not empty")
+      + " authenticationRequest is " + (StringUtils.isEmpty(authenticationRequest) ? "empty" : "not empty")
+      + " showAuthenticationRequest is " + showAuthenticationRequest);
   }
 
-  public void handleCallback()
+  public void testAction()
+  {
+    System.out.println ("########################################################");
+  }
+
+  /*public void handleCallback()
   {
     Map<String, String> params = facesContext.getExternalContext()
       .getRequestParameterMap();
@@ -245,9 +140,21 @@ public class OpenIdConnectView implements Serializable
     {
       exchangeCodeForTokens();
     }
+  }*/
+
+  public void onTabChange(TabChangeEvent event)
+  {
+    FacesMessage msg = new FacesMessage("Tab Changed", "Active Tab: " + event.getTab().getTitle());
+    FacesContext.getCurrentInstance().addMessage(null, msg);
   }
 
-  private void exchangeCodeForTokens()
+  public void onTabClose(TabCloseEvent event)
+  {
+    FacesMessage msg = new FacesMessage("Tab Closed", "Closed tab: " + event.getTab().getTitle());
+    FacesContext.getCurrentInstance().addMessage(null, msg);
+  }
+
+  /*private void exchangeCodeForTokens()
   {
     Client client = ClientBuilder.newClient();
     Form form = new Form()
@@ -267,14 +174,13 @@ public class OpenIdConnectView implements Serializable
       refreshToken = (String) tokens.get("refresh_token");
       idToken = (String) tokens.get("id_token");
       accessToken = (String) tokens.get("access_token");
-    }
-    catch (Exception e)
+    } catch (Exception e)
     {
       FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR,
         "Error exchanging code for tokens", e.getMessage());
       facesContext.addMessage(null, message);
     }
-  }
+  }*/
 
   private String getRedirectUri()
   {
